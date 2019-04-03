9afee67e354d598979c4812323509048e0b2e315
RivkaLeichter <rvktmr@gmail.com>
2019-04-03 21:17:38 +0300

    Add new componnent Member

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
 import { FormsModule } from '@angular/forms';
+import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookDetailsComponent,
-    BookEditComponent
+    BookEditComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule,
